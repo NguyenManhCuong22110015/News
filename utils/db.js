4f8fa5376b19b27f5826bf6316a06a75747ebf9d
Cuong <121237768+NMCuonG08@users.noreply.github.com>
2025-02-13 21:02:09 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ import knexObj from "knex"
 const db = knexObj({
     client: 'mysql2',
     connection:{
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: 'cuong1182004',
-        database: 'testdb'
+        host: 'dpg-cumvku8gph6c738amcsg-a',
+        port: '5432',
+        user: 'cuong_user',
+        password: 'pyu0JR6meTD2UCor1FUjFLZKv8N1JMpt',
+        database: 'cuong'
     },
     pool : {min: 0, max: 7}
 })
