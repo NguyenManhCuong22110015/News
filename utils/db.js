b87baef5812e8d5f2e511f421d173621b2cffdde
Your Name <you@example.com>
2025-02-23 22:48:00 +0700

    update

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ import knexObj from "knex"
 const db = knexObj({
     client: 'mysql2',
     connection:{
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: 'cuong1182004',
-        database: 'tt'
+        host: 'salu0.h.filess.io',
+        port: '3307',
+        user: 'News_makingfox',
+        password: '40f9046bcd54c93d96a48d2277c336f08f111b3d',
+        database: 'News_makingfox'
     },
     pool : {min: 0, max: 7}
 })
